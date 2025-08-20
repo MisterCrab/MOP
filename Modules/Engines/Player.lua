4cd7f34177a07720ad7c2fe6bde23c0a6202d3b0
Ayni <51509374+MisterCrab@users.noreply.github.com>
2025-08-20 20:46:38 +0300

    Added Death presence into Rune API

--- Modules/Engines/Player.lua
+++ Modules/Engines/Player.lua
@@ -161,9 +161,10 @@ local Data = {
 	InfoInv 	= {},
 	-- Runes
 	RunePresence = {
-		[CONST.DEATHKNIGHT_BLOOD] = 1, 	Blood = 1, 
-		[CONST.DEATHKNIGHT_FROST] = 3, 	Frost = 3,
-		[CONST.DEATHKNIGHT_UNHOLY] = 2, Unholy = 2,
+		[CONST.DEATHKNIGHT_BLOOD] 	= 1, 	Blood 	= 1, 
+		[CONST.DEATHKNIGHT_FROST] 	= 3, 	Frost 	= 3,
+		[CONST.DEATHKNIGHT_UNHOLY] 	= 2, 	Unholy 	= 2,
+											Death 	= 4,
 	},	
 	-- Glyph
 	Glyphs		= {},
@@ -1532,7 +1533,7 @@ end
 
 -- rune
 function Player:Rune(presence)
-	local presenceType = DataRunePresence[presence]	
+	local presenceType = DataRunePresence[presence]	or presence
     local c = 0
 
 	local runeType
